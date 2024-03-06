93e13cecee906975b0f2948c40bf398f5da399d5
Martin Larralde <martin.larralde@embl.de>
2024-03-06 09:20:21 +0100

    Remove unused import from `pybarrnap.record`

--- src/pybarrnap/record.py
+++ src/pybarrnap/record.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Iterable
 
 from Bio.SeqFeature import SeqFeature, SimpleLocation
 from pyhmmer.plan7 import Hit
